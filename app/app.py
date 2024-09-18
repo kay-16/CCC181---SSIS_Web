8087e406f236e8acd51ea2df3f423d582a582612
Kyla Reambonanza <20221465@gmail.com>
2024-09-18 15:51:04 +0800

    minor changes in templates

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask
 from config import SECRET_KEY, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, BOOTSTRAP_SERVE_LOCAL
 #from flask_mysqldb import MySQL
 
@@ -10,12 +10,6 @@ app.config['MYSQL_USER'] = DB_USERNAME
 app.config['MYSQL_PASSWORD'] = DB_PASSWORD
 app.config['MYSQL_HOST'] = DB_HOST
 app.config['BOOTSTRAP_SERVE_LOCAL'] = BOOTSTRAP_SERVE_LOCAL
-
-#app.config['DB_NAME'] = DB_NAME
-#app.config['DB_USERNAME'] = DB_USERNAME
-#app.config['DB_PASSWORD'] = DB_PASSWORD
-#app.config['DB_HOST'] = DB_HOST
-#app.config['BOOTSTRAP_SERVE_LOCAL'] = BOOTSTRAP_SERVE_LOCAL
 
 #mysql = MySQL(app)
 
