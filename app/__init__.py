c1b8db5a3aa3ecbe739db094f71ee0cc508aa424
Kyla Reambonanza <20221465@gmail.com>
2024-09-30 00:57:35 +0800

    Final (?) set up for __init__.py and app.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,43 @@
 from flask import Flask
+from config import SECRET_KEY, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, BOOTSTRAP_SERVE_LOCAL
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
+mysql = MySQL()
+csrf = CSRFProtect()
 
-mysql = MySQL(app)
+def create_app():
+    app = Flask(__name__)
+    
+    # Load configurations
+    app.config.from_object('config')
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['MYSQL_DB'] = DB_NAME
+    app.config['MYSQL_USER'] = DB_USERNAME
+    app.config['MYSQL_PASSWORD'] = DB_PASSWORD
+    app.config['MYSQL_HOST'] = DB_HOST
+    app.config['MYSQL_PORT'] = DB_PORT
+    app.config['BOOTSTRAP_SERVE_LOCAL'] = BOOTSTRAP_SERVE_LOCAL
+    app.config['MYSQL_DATABASE_SOCKET'] = ' '
 
-# Importing routes from all the packages
-from app.app import app                 
-from app.students.routes import students
-from app.programs.routes import programs
-from app.college.routes import college
-from app.register.routes import register
-from app.login.routes import login
-from app.main.routes import main
+    # Initialize extensions
+    mysql.init_app(app)
+    csrf.init_app(app)
 
-# Register blueprints
-app.register_blueprint(students)
-app.register_blueprint(programs)
-app.register_blueprint(college)
-app.register_blueprint(register)
-app.register_blueprint(login)
-app.register_blueprint(main)
+    # Importing routes from all the packages
+    from app.students.routes import students
+    from app.programs.routes import programs
+    from app.college.routes import college
+    from app.register.routes import register
+    from app.login.routes import login
+    from app.main.routes import main
 
+    # Register blueprints
+    app.register_blueprint(students)
+    app.register_blueprint(programs)
+    app.register_blueprint(college)
+    app.register_blueprint(register)
+    app.register_blueprint(login)
+    app.register_blueprint(main)
 
-CSRFProtect(app)
+    return app
--- app/app.py
+++ app/app.py
@@ -1,16 +1,6 @@
-from flask import Flask
-from config import SECRET_KEY, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, BOOTSTRAP_SERVE_LOCAL
-from flask_mysqldb import MySQL
+from app import create_app
 
+app = create_app()  # Create the Flask app
 
-app = Flask(__name__) #__name__ (name of module)
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config['MYSQL_DB'] = DB_NAME
-app.config['MYSQL_USER'] = DB_USERNAME
-app.config['MYSQL_PASSWORD'] = DB_PASSWORD
-app.config['MYSQL_HOST'] = DB_HOST
-app.config['MYSQL_PORT'] = DB_PORT
-app.config['BOOTSTRAP_SERVE_LOCAL'] = BOOTSTRAP_SERVE_LOCAL
-app.config['MYSQL_DATABASE_SOCKET'] = ' '
-
-mysql = MySQL(app)
+if __name__ == '__main__':
+    app.run(debug=True)
